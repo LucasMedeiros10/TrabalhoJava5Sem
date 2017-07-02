9f102075eb27782c8b0b1b0f42f07d4ebbbeff21
Lucas-PC\Lucas <lucas_medeiros09@hotmail.com>
2017-07-02 00:59:30 -0300

    Envio dos exemplos de projetos do trabalho

---
+++ CDIFactory/src/main/java/br/univel/classes/Cheque.java
@@ -0,0 +1,12 @@
+package br.univel.classes;
+
+import br.univel.interfaces.Pagamento;
+
+public class Cheque implements Pagamento{
+
+	@Override
+	public String getTipoPagamento() {
+		return "Cheque";
+	}
+
+}
---
+++ CDIFactory/src/main/java/br/univel/interfaces/Pagamento.java
@@ -0,0 +1,8 @@
+package br.univel.interfaces;
+
+import br.univel.enums.TipoPagamento;
+
+public interface Pagamento {
+	
+	String getTipoPagamento();
+}
